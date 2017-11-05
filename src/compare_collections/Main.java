802046a2489dbb794f30c3e83f774fca42626675
VikaSannikova <vikasannikova1998@gmail.com>
2017-11-05 19:36:04 +0300

    add conclusions

--- src/compare_collections/Main.java
+++ src/compare_collections/Main.java
@@ -4,8 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Compare.compareLists();
         System.out.println();
+        System.out.println("LinkedList лучше подходит для задач с большим количеством операций вставки, удаления элементов из списка\n" +
+                "В остальных случаях ArrayList выигрывает(при поиске и доступе к элементу) \n");
+        System.out.println();
         Compare.compareMaps();
         System.out.println();
+        System.out.println("LinkedHashMap быстрее осуществляет поиск, добавление удаление элемета\n" +
+                "TreeMap проигрывает в операциях поиска и добавления");
+        System.out.println();
         Compare.compareSets();
-    }
+        System.out.println();
+        System.out.println("TreeSet и LinkedSet быстрее делают вставку элемента, hashSet гораздо хуже\n" +
+                "Поиск элемента в множестве быстрее осуществляет TreeSet, LinkedSet, HashSet хуже\n" +
+                "Удаление элемента из множества быстрее всего выполняется в LinkedSet");
+          }
 }
