75add136492f36728d3287fcdbfcb6474d4df52a
VikaSannikova <vikasannikova1998@gmail.com>
2017-11-05 17:58:14 +0300

    list done

--- src/list/Main.java
+++ src/list/Main.java
@@ -6,6 +6,75 @@ import java.util.List;
 import java.util.Random;
 
 public class Main {
+    public static void compare(){
+         MyLinkedList<Integer> myList = new MyLinkedList<>();
+         List<Integer> list = new LinkedList<>();
+         int tmp = 0;
+         for(int i = 0; i < 100; i++) {
+             tmp = (int)(Math.random() * 100);
+             list.add(tmp);
+             myList.add(tmp);
+         }
+
+         long startTime = System.nanoTime();
+         list.add(47, 9);
+         long estimatedTime = System.nanoTime() - startTime;
+         System.out.println("adding in LinkedList: " + estimatedTime);
+
+         startTime = System.nanoTime();
+         myList.add(47, 9);
+         estimatedTime = System.nanoTime() - startTime;
+         System.out.println("adding in MyLinkedList: " + estimatedTime);
+
+         ///////////////////////////////////////////////////////////////
+
+         startTime = System.nanoTime();
+         list.get(47);
+         estimatedTime = System.nanoTime() - startTime;
+         System.out.println("get element from in LinkedList: " + estimatedTime);
+
+         startTime = System.nanoTime();
+         myList.get(47);
+         estimatedTime = System.nanoTime() - startTime;
+         System.out.println("get element from in MyLinkedList: " + estimatedTime);
+
+         ///////////////////////////////////////////////////////////////
+
+         startTime = System.nanoTime();
+         list.indexOf(9);
+         estimatedTime = System.nanoTime() - startTime;
+         System.out.println("indexOf element in LinkedList: " + estimatedTime);
+
+         startTime = System.nanoTime();
+         myList.indexOf(9);
+         estimatedTime = System.nanoTime() - startTime;
+         System.out.println("indexOf element in MyLinkedList: " + estimatedTime);
+
+         //////////////////////////////////////////////////////////////
+
+         startTime = System.nanoTime();
+         /*tmp = */list.remove(47);
+         estimatedTime = System.nanoTime() - startTime;
+         System.out.println("remove element from LinkedList: " + estimatedTime/*+ " tmp = " +tmp*/);
+
+         startTime = System.nanoTime();
+         /*tmp = */myList.remove(47);
+         estimatedTime = System.nanoTime() - startTime;
+         System.out.println("remove element from MyLinkedList: " + estimatedTime/*+ " tmp = " +tmp*/);
+
+         //////////////////////////////////////////////////////////////
+
+         startTime = System.nanoTime();
+         list.clear();
+         estimatedTime = System.nanoTime() - startTime;
+         System.out.println("clear LinkedList: " + estimatedTime);
+
+         startTime = System.nanoTime();
+         myList.clear();
+         estimatedTime = System.nanoTime() - startTime;
+         System.out.println("clear MyLinkedList: " + estimatedTime);
+     }
+
     public static void main(String[] args) {
         MyLinkedList<Integer> list = new MyLinkedList<>();
         int value = 0;
@@ -39,5 +108,9 @@ public class Main {
         System.out.println(list.toString());
         System.out.println("get(2) = " + list.get(2));
 
+        list.clear();
+        System.out.println("i'm not-->"+list.toString()+"<--here.");
+
+        compare();
     }
 }
