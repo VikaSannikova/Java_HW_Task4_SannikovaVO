6eea15aacbd9e18cdf253c9cce1bf193e8542c13
VikaSannikova <vikasannikova1998@gmail.com>
2017-11-06 20:00:12 +0300

    edit conclusions

--- src/compare_collections/Compare.java
+++ src/compare_collections/Compare.java
@@ -1,151 +1,94 @@
 package compare_collections;
 
+import javax.swing.*;
 import java.util.*;
 
 public class Compare
 {
-    public static void compareLists(){
-        List<Integer> arrayList = new ArrayList<Integer>();
-        List<Integer> linkedList = new LinkedList<Integer>();
-
+    public static void compareLists(List<Integer>list){
         int tmp = 0;
         for(int i = 0; i < 10000; i++) {
             tmp = (int)Math.random()*100;
-            arrayList.add(tmp);
-            linkedList.add(tmp);
+            list.add(tmp);
         }
 
         long startTime = System.nanoTime();
-        arrayList.add(47, 9);
+        list.add(47, 9);
         long estimatedTime = System.nanoTime() - startTime;
-        System.out.println("adding in ArrayList: " + estimatedTime);
-
-        startTime = System.nanoTime();
-        linkedList.add(47, 9);
-        estimatedTime = System.nanoTime() - startTime;
-        System.out.println("adding in LinkedList: " + estimatedTime);
+        System.out.println("adding in "+list.getClass().getName()+": " + estimatedTime);
 
         ////////////////////////////////////////////////////////////
 
         startTime = System.nanoTime();
-        arrayList.get(47);
+        list.get(47);
         estimatedTime = System.nanoTime() - startTime;
-        System.out.println("getting element from ArrayList: " + estimatedTime);
-
-        startTime = System.nanoTime();
-        linkedList.get(47);
-        estimatedTime = System.nanoTime() - startTime;
-        System.out.println("getting element from LinkedList: " + estimatedTime);
+        System.out.println("getting element from "+list.getClass().getName()+": " + estimatedTime);
 
         ///////////////////////////////////////////////////////////
 
         startTime = System.nanoTime();
-        arrayList.indexOf(9);
+        list.indexOf(9);
         estimatedTime = System.nanoTime() - startTime;
-        System.out.println("indexOf element from ArrayList: " + estimatedTime);
-
-        startTime = System.nanoTime();
-        linkedList.indexOf(9);
-        estimatedTime = System.nanoTime() - startTime;
-        System.out.println("indexOf element from LinkedList: " + estimatedTime);
+        System.out.println("indexOf element from "+list.getClass().getName()+": " + estimatedTime);
 
         ///////////////////////////////////////////////////////////
 
         startTime = System.nanoTime();
-        arrayList.remove(47);
+        list.remove(47);
         estimatedTime = System.nanoTime() - startTime;
-        System.out.println("remove from ArrayList (begin): " + estimatedTime);
-
-        startTime = System.nanoTime();
-        linkedList.remove(47);
-        estimatedTime = System.nanoTime() - startTime;
-        System.out.println("remove from LinkedList (begin): " + estimatedTime);
+        System.out.println("remove from "+list.getClass().getName()+": " + estimatedTime);
     }
 
-    public static void compareSets(){
-        Set<Integer> hashSet = new HashSet<Integer>();
-        Set<Integer> linkedSet = new LinkedHashSet<Integer>();
-        Set<Integer> treeSet = new TreeSet<Integer>();
+    public static void compareSets(Set<Integer>set){
         int tmp = 0;
         for(int i = 0; i <10000; i++) {
             tmp = (int)Math.random()*100;
-            hashSet.add(tmp);
-            linkedSet.add(tmp);
-            treeSet.add(tmp);
+            set.add(tmp);
         }
 
         long startTime, estimatedTime;
         startTime = System.nanoTime();
-        hashSet.add(47);
+        set.add(47);
         estimatedTime = System.nanoTime() - startTime;
-        System.out.println("adding in hashSet: " + estimatedTime);
-
-        startTime = System.nanoTime();
-        linkedSet.add(47);
-        estimatedTime = System.nanoTime() - startTime;
-        System.out.println("adding in linkedSet: " + estimatedTime);
-
-        startTime = System.nanoTime();
-        treeSet.add(47);
-        estimatedTime = System.nanoTime() - startTime;
-        System.out.println("adding in treeSet: " + estimatedTime);
+        System.out.println("adding in "+set.getClass().getName()+": " + estimatedTime);
 
         /////////////////////////////////////////////////////////
 
         startTime = System.nanoTime();
-        hashSet.contains(47);
+        set.contains(47);
         estimatedTime = System.nanoTime() - startTime;
-        System.out.println("check contains in hashSet: " + estimatedTime);
-
-        startTime = System.nanoTime();
-        hashSet.contains(47);
-        estimatedTime = System.nanoTime() - startTime;
-        System.out.println("check contains in linkedSet: " + estimatedTime);
-
-        startTime = System.nanoTime();
-        hashSet.contains(47);
-        estimatedTime = System.nanoTime() - startTime;
-        System.out.println("check contains in treeSet: " + estimatedTime);
+        System.out.println("check contains in "+set.getClass().getName()+": " + estimatedTime);
 
         ///////////////////////////////////////////////////////
 
         startTime = System.nanoTime();
-        hashSet.remove(47);
+        set.remove(47);
         estimatedTime = System.nanoTime() - startTime;
-        System.out.println("removing from hashSet: " + estimatedTime);
+        System.out.println("removing from "+set.getClass().getName()+": " + estimatedTime);
 
-        startTime = System.nanoTime();
-        linkedSet.remove(47);
-        estimatedTime = System.nanoTime() - startTime;
-        System.out.println("removing from linkedSet: " + estimatedTime);
-
-        startTime = System.nanoTime();
-        treeSet.remove(47);
-        estimatedTime = System.nanoTime() - startTime;
-        System.out.println("removing from treeSet: " + estimatedTime);
     }
 
-    public static void compareMaps(){
-        Map<Integer, Double> hashMap = new HashMap<Integer, Double>();
+    public static void compareMaps(Map<Integer,Double>map){
+        /*Map<Integer, Double> hashMap = new HashMap<Integer, Double>();
         Map<Integer, Double> linkedHashMap = new LinkedHashMap<Integer, Double>();
-        Map<Integer, Double> treeMap = new TreeMap<Integer, Double>();
+        Map<Integer, Double> treeMap = new TreeMap<Integer, Double>();*/
         int tmp = 0;
         double db = 0;
         for(int i = 0; i < 10000; i++) {
             tmp = (int)Math.random()*100;
             db = Math.random()*150;
-            hashMap.put(tmp, db );
-            linkedHashMap.put(tmp, db);
-            treeMap.put(tmp, db);
+            map.put(tmp, db );
+/*            linkedHashMap.put(tmp, db);
+            treeMap.put(tmp, db);*/
         }
 
         long startTime, estimatedTime;
         startTime = System.nanoTime();
-        hashMap.put(47, 9.0);
+        map.put(47, 9.0);
         estimatedTime = System.nanoTime() - startTime;
-        System.out.println("put in hashMap: " + estimatedTime);
+        System.out.println("put in "+map.getClass().getName()+": " + estimatedTime);
 
-        startTime = System.nanoTime();
+        /*startTime = System.nanoTime();
         linkedHashMap.put(47, 9.0);
         estimatedTime = System.nanoTime() - startTime;
         System.out.println("put in linkedHashMap: " + estimatedTime);
@@ -153,16 +96,16 @@ public class Compare
         startTime = System.nanoTime();
         treeMap.put(47, 9.0);
         estimatedTime = System.nanoTime() - startTime;
-        System.out.println("put in treeMap: " + estimatedTime);
+        System.out.println("put in treeMap: " + estimatedTime);*/
 
         ////////////////////////////////////////////////////////
 
         startTime = System.nanoTime();
-        hashMap.containsValue(9.0);
+        map.containsValue(9.0);
         estimatedTime = System.nanoTime() - startTime;
-        System.out.println("check hashMap: " + estimatedTime);
+        System.out.println("check "+map.getClass().getName()+": " + estimatedTime);
 
-        startTime = System.nanoTime();
+        /*startTime = System.nanoTime();
         linkedHashMap.containsValue(9.0);
         estimatedTime = System.nanoTime() - startTime;
         System.out.println("check linkedHashMap: " + estimatedTime);
@@ -170,16 +113,16 @@ public class Compare
         startTime = System.nanoTime();
         treeMap.containsValue(9.0);
         estimatedTime = System.nanoTime() - startTime;
-        System.out.println("check treeMap: " + estimatedTime);
+        System.out.println("check treeMap: " + estimatedTime);*/
 
         ////////////////////////////////////////////////////////
 
         startTime = System.nanoTime();
-        hashMap.remove(47);
+        map.remove(47);
         estimatedTime = System.nanoTime() - startTime;
-        System.out.println("remove from hashMap: " + estimatedTime);
+        System.out.println("remove from "+map.getClass().getName()+": " + estimatedTime);
 
-        startTime = System.nanoTime();
+        /*startTime = System.nanoTime();
         linkedHashMap.remove(47);
         estimatedTime = System.nanoTime() - startTime;
         System.out.println("remove from linkedHashMap: " + estimatedTime);
@@ -187,6 +130,6 @@ public class Compare
         startTime = System.nanoTime();
         treeMap.remove(47);
         estimatedTime = System.nanoTime() - startTime;
-        System.out.println("remove from treeMap: " + estimatedTime);
+        System.out.println("remove from treeMap: " + estimatedTime);*/
     }
 }
--- src/compare_collections/Main.java
+++ src/compare_collections/Main.java
@@ -1,21 +1,35 @@
 package compare_collections;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        Compare.compareLists();
+        Compare.compareLists(new ArrayList<Integer>());
+        Compare.compareLists(new LinkedList<Integer>());
+
         System.out.println();
         System.out.println("LinkedList лучше подходит для задач с большим количеством операций вставки, удаления элементов из списка\n" +
                 "В остальных случаях ArrayList выигрывает(при поиске и доступе к элементу) \n");
         System.out.println();
-        Compare.compareMaps();
-        System.out.println();
-        System.out.println("LinkedHashMap быстрее осуществляет поиск, добавление удаление элемета\n" +
-                "TreeMap проигрывает в операциях поиска и добавления");
-        System.out.println();
-        Compare.compareSets();
+
+        Compare.compareSets(new HashSet<Integer>());
+        Compare.compareSets(new LinkedHashSet<Integer>());
+        Compare.compareSets(new TreeSet<Integer>());
+
         System.out.println();
         System.out.println("TreeSet и LinkedSet быстрее делают вставку элемента, hashSet гораздо хуже\n" +
                 "Поиск элемента в множестве быстрее осуществляет TreeSet, LinkedSet, HashSet хуже\n" +
                 "Удаление элемента из множества быстрее всего выполняется в LinkedSet");
+        System.out.println();
+
+        Compare.compareMaps(new HashMap<Integer, Double>());
+        Compare.compareMaps(new LinkedHashMap<Integer, Double>());
+        Compare.compareMaps(new TreeMap<Integer, Double>());
+        System.out.println();
+        System.out.println("LinkedHashMap быстрее осуществляет поиск, добавление удаление элемета\n" +
+                "TreeMap проигрывает в операциях поиска и добавления");
+        System.out.println();
+
+
           }
 }
